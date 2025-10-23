fb06bd76efd63496a753fee7ad675f88caabe2fd
Margarita5112002 <margaritaalcarazrojas@gmail.com>
2025-10-23 12:44:59 -0700

    style register page

--- Frontend/src/app/features/auth/register/register.component.ts
+++ Frontend/src/app/features/auth/register/register.component.ts
@@ -1,10 +1,18 @@
 import { Component } from "@angular/core";
+import { MatIconModule } from "@angular/material/icon";
+import { RouterLink, RouterLinkActive } from "@angular/router";
 
 @Component({
     selector: 'app-register',
     templateUrl: './register.component.html',
-    styleUrl: './register.component.css'
+    styleUrl: './register.component.css',
+    imports: [MatIconModule, RouterLinkActive, RouterLink]
 })
 export class RegisterComponent {
-    
+    passwordVisible = false
+
+    togglePasswordVisibility() {
+        this.passwordVisible = !this.passwordVisible
+    }
+
 }
